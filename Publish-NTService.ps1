fe6878c44e1fae7e789986ad8fce4a79c9ab469f
Mike <mischul@microsoft.com>
2020-05-01 01:20:50 -0700

    Nuget build file (nuspec)

---
+++ Publish-NTService.ps1
@@ -0,0 +1,14 @@
+﻿<#
+.SYNOPSIS
+    Builds the Device Announcer projects App and Service and publishes them to the Release folder.
+    Note: if you see "failed to get information" from nuget site, rerunning several times may fix this.
+#>
+Param
+(
+    [String]$OutputPath="Publish"
+)
+Write-Host "Publish: Publishing svc to location: " $OutputPath
+
+dotnet publish MikeService -r win-x64 -c Release -o .\Published\win-x64
+
+Write-Host "Publish: Complete!"
